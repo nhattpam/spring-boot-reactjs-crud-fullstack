3031c0171a14f3378b56cac215dc32b87f2088c7
Nhat Pham <nhat2up@gmail.com>
2022-12-31 10:02:12 +0700

    add user

--- crud-front-end/src/App.js
+++ crud-front-end/src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddUser from './users/AddUser';
 function App() {
   return (
     <div className="App">
+      <Router>
         <Navbar />
-        <Home />
+
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/add-user" element={<AddUser/>} />
+        </Routes>
+
+      </Router>
     </div>
   );
 }
--- crud-front-end/src/layout/Navbar.js
+++ crud-front-end/src/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
@@ -10,9 +11,9 @@ export default function Navbar() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <button className='btn btn-outline-light'>
+                    <Link className='btn btn-outline-light' to="/add-user">
                         Add User
-                    </button>
+                    </Link>
                 </div>
             </nav>
         </div>
